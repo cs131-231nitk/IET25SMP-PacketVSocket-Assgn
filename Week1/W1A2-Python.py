aa753367aefa150b21d6a1204ec24b352764cdc5
jumbo-pruto <jumbo.pruto@gmail.com>
2025-06-09 18:28:15 +0530

    Added Week1:Assignments

---
+++ Week1/W1A2-Python.py
@@ -0,0 +1,39 @@
+# Week-1 Assignment-2 | Python
+
+'''
+---
+SIMPLE QUIZ GAME!
+---
+>> PROBLEM STATEMENT:
+    Write a Python program that runs a simple multiple-choice quiz for the user.
+
+>> REQUIREMENTS:
+    - The quiz should consist of at least 3 questions.
+    - Each question should have:
+        - A text prompt
+        - Three answer choices (a/b/c)
+        - One correct answer
+    - The user inputs their answer (a/b/c) for each question.
+    - After each answer, the program tells the user whether they were correct.
+    - At the end of the quiz, display the user's total score out of the number of questions.
+
+>> EXAMPLE RUN:
+
+    Q1: What is the capital of France?  
+    a) Berlin  
+    b) Paris  
+    c) Madrid  
+    Your answer: {b} <- User_input  
+    Correct!
+
+    Q2: What is 5 * 6?  
+    a) 30  
+    b) 20  
+    c) 25  
+    Your answer: {a} <- User_input
+    Correct!
+
+
+~Mentored by: Dhiya N
+
+'''
